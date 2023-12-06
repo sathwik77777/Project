c01f16bcd98c2cd322c5c1be3e86f64690721af2
sathwik77777 <93912523+sathwik77777@users.noreply.github.com>
2023-12-07 01:10:53 +0530

    Add files via upload

---
+++ Controllers/SellerController.cs
@@ -0,0 +1,129 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FashionHexa.Entities;
+using FashionHexa.Services;
+using FashionHexa.Models;
+using FashionHexa.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using log4net;
+using System.Runtime.ConstrainedExecution;
+
+namespace FashionHexa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+
+        private readonly ISellerService sellerService;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration configuration;
+        private ILogger<SellerController> logger;
+        public SellerController(ISellerService sellerService, IMapper mapper, IConfiguration configuration, ILogger<SellerController> logger)
+        {
+            this.sellerService = sellerService;
+            _mapper = mapper;
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+
+        [HttpGet, Route("GetAllSellers")]
+        //[Authorize(Roles = "Admin")]
+        public IActionResult GetAllSellers()
+        {
+            try
+            {
+                List<Seller> sellers = sellerService.GetAllSeller();
+                List<SellerDTO> sellerDTO = _mapper.Map<List<SellerDTO>>(sellers);
+                return StatusCode(200, sellers);
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost, Route("Register Seller")]
+       // [AllowAnonymous] //access the endpoint any any user with out login
+        public IActionResult AddSeller(SellerDTO sellerDTO)
+        {
+            try
+            {
+                Seller seller = _mapper.Map<Seller>(sellerDTO);
+                sellerService.AddSeller(seller);
+                return StatusCode(200, seller);
+
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut, Route("EditSeller")]
+       // [Authorize(Roles = "Seller")]
+        public IActionResult EditSeller(SellerDTO sellerDTO)
+        {
+            try
+            {
+                Seller seller = _mapper.Map<Seller>(sellerDTO);
+                sellerService.UpdaterSeller(seller);
+                return StatusCode(200, seller);
+
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete, Route("DeleteSeller/{sellerId}")]
+       // [Authorize(Roles = "Admin")]
+        public IActionResult DeleteSeller(int sellerId)
+        {
+            try
+            {
+                sellerService.Deleteseller(sellerId);
+                return StatusCode(200, new JsonResult($"User with Id {sellerId} is Deleted"));
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet, Route("GetSellerById/{SellerId}")]
+       // [Authorize(Roles ="Admin")]
+        public IActionResult SellerById(int sellerId)
+        {
+            try
+            {
+                Seller seller = sellerService.GetSellerById(sellerId);
+                SellerDTO sellerDTO = _mapper.Map<SellerDTO>(seller);
+                if (seller != null)
+                    return StatusCode(200, seller);
+                else
+                    return StatusCode(404, new JsonResult("Invalid Id"));
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ DTO/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace FashionHexa.DTO
+{
+    public class UserDTO
+    {
+        /// <summary>
+        /// public int UserId { get; set; }
+        /// </summary>
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        
+        public string? Mobile { get; set; }
+        
+
+        //public string Rolename { get; set; }
+    }
+}
